f7d4ad05a6fbc74c196cc5382fa36531599ee491
penguin418 <penguin418@naver.com>
2022-03-12 23:21:33 +0900

    add authroization handler redirection

--- src/main/java/com/github/penguin418/oauth2/provider/dto/AuthorizationRequest.java
+++ src/main/java/com/github/penguin418/oauth2/provider/dto/AuthorizationRequest.java
@@ -1,9 +1,18 @@
 package com.github.penguin418.oauth2.provider.dto;
 
+import com.github.penguin418.oauth2.provider.exception.AuthError;
+import com.github.penguin418.oauth2.provider.exception.AuthException;
+import com.github.penguin418.oauth2.provider.validation.BasicABNFComparator;
+import io.vertx.core.MultiMap;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.ext.web.RoutingContext;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Authorization Request<br>
@@ -14,9 +23,20 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AuthorizationRequest {
+    public static final String SESSION_STORE_NAME = "authorization_request";
     private String responseType;
     private String clientId;
     private String redirectUri;
     private String scope;
     private String state;
+
+    public static AuthorizationRequest fromQuery(HttpServerRequest request) throws AuthException {
+        MultiMap queryParams = request.params();
+        String responseType = queryParams.get("response_type");
+        String clientId = queryParams.get("client_id");
+        String redirectUri = queryParams.get("redirect_uri");
+        String scope = queryParams.get("scope");
+        String state = queryParams.get("state");
+        return new AuthorizationRequest(responseType,clientId,redirectUri,scope,state);
+    }
 }
--- src/main/java/com/github/penguin418/oauth2/provider/handler/AuthorizationHandler.java
+++ src/main/java/com/github/penguin418/oauth2/provider/handler/AuthorizationHandler.java
@@ -1,43 +1,58 @@
 package com.github.penguin418.oauth2.provider.handler;
 
+import com.github.penguin418.oauth2.provider.dto.AuthorizationRequest;
+import com.github.penguin418.oauth2.provider.exception.AuthError;
+import com.github.penguin418.oauth2.provider.service.OAuth2StorageService;
 import com.github.penguin418.oauth2.provider.validation.AuthorizationRequestValidation;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpMethod;
-import io.vertx.core.http.HttpServerRequest;
 import io.vertx.ext.web.RoutingContext;
 
-import static com.github.penguin418.oauth2.provider.exception.AuthError.INVALID_REQUEST;
+import static com.github.penguin418.oauth2.provider.exception.AuthError.*;
 
 public class AuthorizationHandler implements Handler<RoutingContext> {
     private final Vertx vertx;
     private final String login_uri;
 
     private final AuthorizationRequestValidation validator = new AuthorizationRequestValidation();
+    private final OAuth2StorageService storageService;
 
     public AuthorizationHandler(Vertx vertx, String login_uri) {
         this.vertx = vertx;
         this.login_uri = login_uri;
+        this.storageService = OAuth2StorageService.createProxy(vertx);
     }
 
     @Override
     public void handle(RoutingContext event) {
         // 입력값 검사
-        if (!validator.isValidCodeAuthorizationRequest(event))
-            event.fail(INVALID_REQUEST.exception());
-
-        final HttpServerRequest request = event.request();
-        if (request.method().equals(HttpMethod.POST))
-            handlePost(request);
-        else if (request.method().equals(HttpMethod.GET))
-            handleGet(request);
-        else event.fail(INVALID_REQUEST.exception());
+        if (event.queryParams().get("response_type").equals("code")) {
+            if (!validator.isValidCodeAuthorizationRequest(event))
+                event.fail(INVALID_REQUEST.exception());
+            handleCodeGrant(event);
+        } else event.fail(INVALID_REQUEST.exception());
     }
 
-    private void handlePost(HttpServerRequest request) {
+    private void handleCodeGrant(RoutingContext event) {
+        final AuthorizationRequest request = AuthorizationRequest.fromQuery(event.request());
+        storageService.getClientByClientId(request.getClientId())
+                .onSuccess(client -> {
 
-    }
-
-    private void handleGet(HttpServerRequest request) {
+                    String redirectionUri = request.getRedirectUri();
+                    if (redirectionUri == null) {
+                        event.session().put(AuthorizationRequest.SESSION_STORE_NAME, request);
+                        event.redirect(login_uri);
+                    } else if (client.getRedirectUris().contains(redirectionUri)) {
+                        event.session().put(AuthorizationRequest.SESSION_STORE_NAME, request);
+                        event.redirect(login_uri);
+                    } else {
+                        AuthError error = INVALID_REDIRECT_URI.withDetail(redirectionUri);
+                        if (request.getState() != null)
+                            error.withState(request.getState());
+                        event.fail(error.exception());
+                    }
+                }).onFailure(fail->{
+                    event.fail(UNAUTHORIZED_CLIENT.exception());
+                });
     }
 }
