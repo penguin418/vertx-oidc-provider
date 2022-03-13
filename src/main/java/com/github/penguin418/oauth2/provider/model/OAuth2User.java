7bc1e9f67953c81fe3a1fd8d5f5a0d288e0cf8a2
penguin418 <penguin418@naver.com>
2022-03-13 14:39:29 +0900

    add bcrypt encryption

--- src/main/java/com/github/penguin418/oauth2/provider/model/OAuth2User.java
+++ src/main/java/com/github/penguin418/oauth2/provider/model/OAuth2User.java
@@ -1,29 +1,51 @@
 package com.github.penguin418.oauth2.provider.model;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.vertx.codegen.annotations.DataObject;
 import io.vertx.core.json.JsonObject;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 @DataObject
 public class OAuth2User {
+    @Getter @Setter
     @JsonProperty("user_id")
     private String userId;
+    @Getter @Setter
     @JsonProperty("username")
     private String username;
+    @Getter @Setter
     @JsonProperty("password")
     private String password;
 
-    public OAuth2User(JsonObject jsonObject) {
+    private boolean isNew = false;
+
+    public OAuth2User(String username, String password){
+        this.username = username;
+        this.password = password;
+        this.isNew = true;
+    }
+
+    public OAuth2User encrypt(){
+        if (this.isNew) {
+            this.password = BCrypt.withDefaults().hashToString(12, this.password.toCharArray());
+            this.isNew = false;
+        }
+        return this;
+    }
+
+    public boolean verified(String password){
+        return BCrypt.verifyer().verify(password.toCharArray(), this.password).verified;
+    }
+
+    public OAuth2User(JsonObject jsonObject){
         this.userId = jsonObject.getString("user_id");
         this.username = jsonObject.getString("username");
         this.password = jsonObject.getString("password");
     }
 
-    public JsonObject toJson() {
+    public JsonObject toJson(){
         return JsonObject.mapFrom(this);
     }
 }
--- src/main/java/com/github/penguin418/oauth2/provider/model/Oauth2Client.java
+++ src/main/java/com/github/penguin418/oauth2/provider/model/Oauth2Client.java
@@ -1,5 +1,7 @@
 package com.github.penguin418.oauth2.provider.model;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.vertx.codegen.annotations.DataObject;
 import io.vertx.core.json.JsonObject;
@@ -7,32 +9,61 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 
-@Getter
-@Setter
 @DataObject
 public class Oauth2Client {
+    @Getter @Setter
     @JsonProperty("client_id")
     private String clientId;
+    @Getter @Setter
     @JsonProperty("client_secret")
     private String clientSecret;
+    @Getter @Setter
     @JsonProperty("response_types")
     private List<String> responseTypes;
+    @Getter @Setter
     @JsonProperty("scopes")
     private List<String> scopes;
+    @Getter @Setter
     @JsonProperty("redirect_uris")
     private List<String> redirectUris;
 
-    public Oauth2Client(JsonObject jsonObject) {
-        this.clientId = jsonObject.getString("client_id");
-        this.clientSecret = jsonObject.getString("client_secret");
-        this.responseTypes = jsonObject.getJsonArray("response_types").getList();
-        this.scopes = jsonObject.getJsonArray("scopes").getList();
-        this.redirectUris = jsonObject.getJsonArray("redirect_uris").getList();
+    @JsonIgnore
+    private boolean isNew = false;
+
+    public Oauth2Client(List<String> responseTypes, List<String> scopes, List<String> redirectUris){
+        this.clientId = UUID.randomUUID().toString();
+        this.clientSecret =  UUID.randomUUID().toString();
+        this.responseTypes=responseTypes;
+        this.scopes=scopes;
+        this.redirectUris = redirectUris;
+        this.isNew = true;
     }
 
-    public JsonObject toJson() {
+    public Oauth2Client encrypt(){
+        if (this.isNew) {
+            this.clientSecret = BCrypt.withDefaults().hashToString(12, this.clientSecret.toCharArray());
+            this.isNew = false;
+        }
+        return this;
+    }
+
+    public boolean verified(String clientSecret){
+        return BCrypt.verifyer().verify(clientSecret.toCharArray(), this.clientSecret).verified;
+    }
+
+    public Oauth2Client(JsonObject jsonObject){
+        this.clientId=jsonObject.getString("client_id");
+        this.clientSecret=jsonObject.getString("client_secret");
+        this.responseTypes=jsonObject.getJsonArray("response_types").getList();
+        this.scopes=jsonObject.getJsonArray("scopes").getList();
+        this.redirectUris=jsonObject.getJsonArray("redirect_uris").getList();
+    }
+
+    public JsonObject toJson(){
         return JsonObject.mapFrom(this);
     }
 }
